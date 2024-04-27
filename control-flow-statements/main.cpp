d5baf13e82968c6c8d23107ad158f3f3fbc66901
Gopal yadav <yadavgopal6675@gmail.com>
2024-04-27 21:47:45 +0530

    control flow statement basic done

--- control-flow-statements/main.cpp
+++ control-flow-statements/main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+using namespace std;
 int main(){
-   std :: cout << "initialize control flow statements" << std :: endl;
+   cout << "Enter your budget :" << endl;
+   int budget;
+   cin >> budget;
+   if(budget > 2000000){
+    cout << "You can buy scorrpio" << endl;
+   } else{
+    cout << "you can't buy scorpio" << endl;
+   }
+
+   int age = 100;
+   if(age>18){
+    cout << "You can Vote" << endl;
+   } else {
+    cout << "You can't vote" << endl;
+   }
+
+
+   int marks =90;
+   if(marks>90){
+    cout << "A" <<endl;
+   } else if(marks > 80){
+    cout << "B" <<endl;
+   } else if (marks >60){
+    cout <<"D";
+   }else{
+    cout <<"You failed";
+   }
+
+
+
+   int height ;
+   cout << "Enter your height:" << endl;
+   cin >> height;
+   int weight;
+   cout << "Enter your weight:" << endl;
+   cin >> weight;
+
+   if(height >5){
+      if(weight >70){
+        cout << "Your BMI is GOOD.";
+      }
+   }
     return 0 ;
+
 
 }

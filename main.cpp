80180dcf05d226f0c406a828ce85871f8f01555d
Gopal yadav <yadavgopal6675@gmail.com>
2024-04-27 12:21:51 +0530

    starting with basics

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age ;
+    cout << "enter your age :" << endl;
+    //taking input using cin identifiers
+    cin >> age;
+    cout << "your age is :" << age << endl;
+
+    return 0;
+}

9d40f86f426745049ef146da10a6308953104b90
Gopal yadav <yadavgopal6675@gmail.com>
2024-04-28 16:53:11 +0530

    solved loop problems

--- loop/main.cpp
+++ loop/main.cpp
@@ -1,3 +1,89 @@
+#include <iostream>
+using namespace std;
 int main(){
+    for (int i = 0 ; i <= 10; i=i+1){
+        cout << "hello ji" << endl ;
+    }
+
+
+    for(int i = 0; i<=5; i++){
+        if(i==3){
+            break;  //will stop after 3.
+        }
+           cout << i << " ";
+    }
+
+    for(int i = 0 ; i<=5; i++){
+        if(i== 4){
+           
+            continue; //only skip 4.
+        }
+          cout << i << " ";
+    }
+
+    int i =1;
+    while(i<=5){
+        cout << i << " ";
+        i++;
+    }
+
+    ///it will run upto infinity
+    ///until you didn't statisfies  all the values
+    // for( ; ; ){
+    //     cout << "hello"<<" ";
+    // }
+    // int j =1;
+    // while(j){
+    //     cout << "hello" << " ";  //run to infinity
+    // }
+
+    // for(int i = 0 ; i>=-10 ; i--){
+    //     cout << i<<" ";
+    // }
+    // for(int i = 1; i<=10; i++){
+    //     cout << i*7 << " ";
+    // }
+    // for(char i = 'A'; i<='Z';i++){
+    //     cout << i << " ";
+    // }
+    // for(char i = 'a'; i<='z';i++){
+    //     cout << i << " ";
+    // }
     
+    // for( ; ;){
+    //     if( i ==3){
+    //         break;
+    //     }
+    //     cout<< "hello" << " ";
+    // }
+
+    //  for(int i =1 ; ; ){
+    //     if(i==1){
+    //         continue;
+    //     } 
+    //     cout << "h" << " ";
+    //  }
+   
+
+      ///this will run infinite times
+      // answer : 1 wil run at infinite times.
+    //  int k =1;
+    //  for( ; ;) {
+    //     cout << k << " ";
+    //  }
+
+
+      
+     for(int i = 3 ; i ; i--){
+        cout << i << " ";   //print  3 2 1
+     }
+
+     for( int i = 3 ; i-- ; ){
+      cout << i << " ";     //print 2 1 0
+     }
+    return 0;
+    
+    for(int i = 1 ; i++<=3 ; i++){   
+        cout << i << " " ;
+    }
 }

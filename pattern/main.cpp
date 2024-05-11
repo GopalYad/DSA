bf0c4e7a79d8dcd9d36e3fa1c19af6a44779c343
Gopal yadav <yadavgopal6675@gmail.com>
2024-05-11 18:05:12 +0530

    pattern problems solved

--- pattern/main.cpp
+++ pattern/main.cpp
@@ -28,5 +28,124 @@ int main()
         cout<<endl;
       }
 
+
+
+
+    int n ;
+    cout << "enter the value of n :";
+    cin >> n;
+          
+     for(int row = 0 ; row < n; row++)
+    {
+        for(int col  = 0 ; col <row+1 ; col++)
+        {
+            if(col == row){
+                cout << row+1;
+            } else{
+                 cout<<row+1 << "*";
+            }
+         
+        }
+        cout <<endl;
+    }
+    
+    
+     for(int row = 0 ; row < n; row++)
+    {
+        for(int col  = 0 ; col <n-row ; col++)
+        {
+            if(col == n-row-1){
+                cout << n-row-1;
+            } else{
+                 cout<<n-row-1<< "*";
+            }
+         
+        }
+        cout <<endl;
+    }
+    
+    
+    // for(int row = 0 ; row < n; row++)
+    // {
+    //     for(int col  = 0 ; col <2*row+1 ; col++)
+    //     {
+    //       if(col % 2 == 0){
+    //           cout <<row+1;
+    //       } else{
+    //           cout << "*";
+    //       }
+    //     }
+    //     cout <<endl;
+    // }
+    // for(int row = 0 ; row < n ; row++){
+        
+    //     for(int col = 0; col <n-row ; col++){
+    //         cout << "*";
+    //     }
+    
+    //     for(int col  = 0 ; col <row+1; col++)
+    //     {
+    //         cout <<"  ";
+    //     }
+    //       for(int col = 0; col <n-row ; col++){
+    //         cout << "*";
+    //     }
+    //     cout <<endl;
+    // }
+    
+    
+    //   for(int row = 0 ; row < n ; row++){
+        
+    //     for(int col = 0; col <row -1; col++){
+    //         cout << "*";
+    //     }
+    
+    //     for(int col  = 0 ; col <n-row; col++)
+    //     {
+    //         cout <<"  ";
+    //     }
+    //       for(int col = 0; col <row+1; col++){
+    //         cout << "*";
+    //     }
+    //     cout <<endl;
+    // }
+    
+    
+    
+    
+    //  for(int row = 0 ; row < n ; row++ )
+    // {
+    //     for(int col = 0 ; col < n-row-1; col++)
+    //     {
+    //         cout<< " ";
+    //     }
+    //     for(int col = 0 ; col < row+1  ; col++)
+    //     {
+    //      if(col == 0 || col == row){
+    //          cout << "* ";
+    //      } else{
+    //          cout << "  ";
+    //      }
+    //     }
+    //     cout <<endl;
+    // }
+    // for(int row = 0 ; row < n ; row++ )
+    // {
+    //     for(int col = 0 ; col < row; col++)
+    //     {
+    //         cout<< " ";
+    //     }
+    //     for(int col = 0 ; col < n-row  ; col++)
+    //     {
+    //      if(col == 0 || col == n-row -1){
+    //          cout << "* ";
+    //      } else{
+    //          cout << "  ";
+    //      }
+    //     }
+    //     cout <<endl;
+    // }
+
+
 return 0;
 }

7b32f5cc8265e9925fec298f6298f3d735936082
Gopal yadav <yadavgopal6675@gmail.com>
2024-06-15 23:31:48 +0530

    arithematic operator

---
+++ operators/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+
+                                          ///uniary operators starts from here ///
+  //post-decrement means
+     //phele value use karlo
+     //phir decrement kar dena
+    int a = 10;
+    cout << (a--) << " ";  //print 10
+    cout << a  << " ";    //print 9
+     
+    //pre-decrement means
+    //phle decrement karlo
+    //phir use karo
+     int b = 10;
+     cout << (--b) << " ";
+     cout <<b <<  " ";  
+     cout << endl;
+
+    //pre-increment
+    //phir use karlo 
+    //phle increment karlo
+    
+    int c = 10;
+    cout << (c++) << endl;
+    cout << c << " ";
+    
+    //post-increment
+    //phle incremnt karlo
+    //phir use karo
+    int d = 10;
+    cout <<(++d) <<endl;
+    cout <<d<<" ";
+
+                                          ///uniary operators ends from here ///
+
+    
+
+    //Arithematic operators
+    int f= 23;
+    int g= 45;
+    cout << (f+g)<<endl;
+        cout << (f-g)<<endl;
+            cout << (f*g)<<endl;
+                cout << (f%g)<<endl;
+
+                cout << ( 5.0 / 2);
+
+
+
+
+
+      ///bitwise operators
+      cout <<endl;
+      cout << (5&4) << endl;          
+                  
+
+
+       return 0;
+}

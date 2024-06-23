9f8b1c69b9d4e8e8d86573cac36424ac5a2d5660
Gopal yadav <yadavgopal6675@gmail.com>
2024-06-23 21:33:51 +0530

    completed 1d & 2d vectors

--- STL/main.cpp
+++ STL/main.cpp
@@ -3,19 +3,27 @@
 using namespace std;
 int main(){
 
-    vector<int>first;
-    vector<int>second;
-    first.push_back(10);
-    first.push_back(11);
-    first.push_back(12);
-    first.push_back(13);
+    // vector<int>first;
+    // vector<int>second;
+    // first.push_back(10);
+    // first.push_back(11);
+    // first.push_back(12);
+    // first.push_back(13);
 
-  second.push_back(100);
-  second.push_back(200);
-  second.push_back(300);
-  second.push_back(400);
-   first.swap(second);
-   cout<<first[0]<< " "<<first[1]<<first[2]<<first[3]<<first[4]<<endl;
+    //traversing  the vector using iteractor
+    //create an iterator
+  //   vector<int>::iterator it = first.begin();
+  //   while(it != first.end()){
+  //     cout<<*it<<endl<<" ";
+  //     it++;
+  //   }
+
+  // second.push_back(100);
+  // second.push_back(200);
+  // second.push_back(300);
+  // second.push_back(400);
+  //  first.swap(second);
+  //  cout<<first[0]<< " "<<first[1]<<first[2]<<first[3]<<first[4]<<endl;
     
 
 
@@ -57,8 +65,38 @@ int main(){
 
 
 
-  vector<int>age(100);
-  age[0] = 10;
+  // vector<int>age(100);
+  // age[0] = 10;
 
+
+  //for each loop
+  // for(int i : first){
+  //   cout<< i<<endl;
+  // }
+  // for(int i : second){
+  //   cout<<i <<endl;
+  // }
+
+
+  ///2D array / vector
+  vector<vector<int>>arr(5,vector<int>(4,0));
+  int totalRows = arr.size();
+  int totalColumns = arr[0].size();
+  //2d array created with 
+  //with 5 rows
+  //with 4 column
+  //with initial value of each cell as '0'
+ 
+
+ ///zaged array
+ vector<vector<int>>brr(4);
+ brr[0]=vector<int>(4);
+ brr[1]=vector<int>(2);
+ brr[2]=vector<int>(3);
+ brr[3]=vector<int>(2);
+ int totRows = brr.size();
+cout<<totRows<<endl;
+int firstColumn = brr[0].size();
+cout<<firstColumn<<endl;
     return 0;
 }

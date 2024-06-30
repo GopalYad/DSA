b217e9c4207234d99726352ffe199d523d941b4d
Gopal yadav <yadavgopal6675@gmail.com>
2024-06-30 10:32:40 +0530

    set bit

---
+++ Bitwise/setBit.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n = 15;
+int countBit = 0;
+
+while(n!=0){
+ int lastNum = (n &1);
+ if(lastNum == 1){
+    countBit++;
+ }
+ n = n >>1;
+}
+cout<<countBit<<endl;
+
+
+
+    return 0;
+
+}

9bf33765c4025b656491539417880489d98509b2
Gopal yadav <yadavgopal6675@gmail.com>
2024-07-01 15:41:48 +0530

    intro to strings

---
+++ CharArrays&Strings/StringsArray/code.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+  string name = "Maharana Pratap";
+  cout<<name[0]<<endl;
+  cout<<name.at(0)<<endl;
+
+
+  cout<<name.front()<<endl;
+  cout<<name.back()<<endl;
+
+
+
+
+
+//     //creation
+// string str;
+
+// str.push_back('g');
+// str.push_back('o');
+// str.push_back('p');
+// str.push_back('a');
+// str.push_back('l');
+// cout<<str<<endl;
+
+
+// string sentence;
+
+// //input
+// getline(cin,sentence,'\n');
+// cout<<sentence<<endl;
+
+    return 0;
+}

1230d0a434e95d19c69810e7c1d90fc53a10e193
Gopal yadav <yadavgopal6675@gmail.com>
2024-08-30 05:20:44 +0530

    update two sum

--- Revision/twoSum.cpp
+++ Revision/twoSum.cpp
@@ -1,21 +1,24 @@
+
 #include <iostream>
 using namespace std;
-bool twoSum(int arr [ ] , int n , int k){
-    for(int i = 0 ;  i < n ;  i++){
-        for(int j = i +1 ; j < n  ; j++){
-            if(arr[ i] +arr[j]==k){
-                return true;
+pair<int , int> checkTwoSum(int arr[ ] , int n , int k){
+    pair <int , int> ans =  make_pair(-1,-1);
+    for(int i = 0 ;  i < n  ; i++){
+        for(int j = i+1 ; j <  n ;  j++ ){
+            if(arr[i]+arr[j]==k){
+                ans.first =arr[i];
+                ans.second=arr[j];
+                return ans;
             }
         }
     }
-    return false;
+    return ans;
 }
 int main() {
-   int arr [] = {10 ,20,30,40,50};
-   int size =  5;
-   int target = 70;
-  bool res = twoSum(arr,size,target);
-  cout<<res<<endl;
+  int arr[] ={10,20,30,40};
+  int size = 4;
+  pair<int , int> ans = checkTwoSum(arr,size,50);
+  cout<<ans.first<<","<<ans.second<<endl;
 
     return 0;
 }

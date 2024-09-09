7a43095c061e1e965c82157ef8549a40d6282626
Gopal yadav <yadavgopal6675@gmail.com>
2024-09-09 18:48:23 +0530

    sloved reverse string

---
+++ Revision/reversesStringinWord.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to reverse words in a given string.
+    string reverseWords(string S) 
+    { 
+        // code here 
+        int r;
+        int len = S.length();
+        string S2 = "";
+        for(int i = len-1 ; i>=0; i--){
+            if(S[i]=='.'){
+                r=len-i;
+                len=i-1;
+                S2.append(S.substr(i+1,r));
+                S2.append(".");
+            }
+            
+        }
+        S2.append(S.substr(0,len+1));
+        return S2;
+    } 
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        string s;
+        cin >> s;
+        Solution obj;
+        cout<<obj.reverseWords(s)<<endl;
+    }
+}
+// } Driver Code Ends

20f918ad25a868f161c97ccdbf7eea7cc5d8e391
Gopal yadav <yadavgopal6675@gmail.com>
2024-12-30 21:44:42 +0530

    selection sort

--- Sortings/selection-sort/main.cpp
+++ Sortings/selection-sort/main.cpp
@@ -1,31 +1,31 @@
-// Online C++ compiler to run C++ program online
+
 #include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
-void selection_sort(vector <int> &arr){
-    //step : select the min elemen and swap
-    int n = arr.size();
-    for(int i = 0 ;  i <= n-2 ; i++){
-        int minIdx = i;
-        for(int j = i+1 ; j <n; j++){
-            if(arr[j]<arr[minIdx]){
-                minIdx = j;
-            }
-        }
-          swap(arr[i], arr[minIdx]);
+
+void sortingAlgorithm(int arr[] , int n){
+   
+    for(int i = 0  ; i <= n-2 ; i++){
+        int min = i;
+      for(int j = i ;  j<=n-1; j++){
+          if(arr[j]<arr[min]){
+              min =j;
+          }
+       
+      }
+       swap(arr[i],arr[min]);
+         
     }
-}
-void printArray(const vector<int> &arr) {  // Use const reference for better efficiency
-    for (const int &val : arr) {
-        cout << val << " ";
+    for(int i = 0 ;  i <=n-1; i++){
+        cout<<arr[i]<<" ";
     }
-    cout << endl;  
+
 }
 int main() {
- vector<int>arr = {1,5,8,9,7,3,0};
- selection_sort(arr);
- printArray(arr);
-  
-
+    // Write C++ code here
+    int arr [] =  {-3, 0, -1, -3, 2, 0, 1};
+    int n = 7;
+    sortingAlgorithm(arr,n);
+   
     return 0;
 }
